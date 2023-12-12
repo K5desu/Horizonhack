740b9a49dd906eeb25bdc0d1c52d11e748fbb540
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2023-12-13 01:50:20 +0900

    reset-vercelrepozitory

--- prisma/migrations/20231212152305_createimg/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `img` to the `Work` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Work" ADD COLUMN     "img" TEXT NOT NULL;
