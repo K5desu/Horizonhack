1ca30a2e41191d1cd4ce1a8a6e0915a52f217512
Riku-mono <riitkou@gmail.com>
2023-12-18 07:03:35 +0900

    Remove AuthButton component from HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Image from 'next/image'
-import AuthButton from './components/AuthButton'
 import Link from 'next/link'
 
 export default async function HomePage() {
   return (
     <>
-      <AuthButton /> <br />
       <h1>Home</h1>
       <p>説明：ホームページ</p>
       <p>リンク例</p>
