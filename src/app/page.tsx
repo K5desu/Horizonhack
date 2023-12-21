908192a61620fa3b8f530d711734211dfb6425f5
Riku-mono <riitkou@gmail.com>
2023-12-21 09:29:31 +0900

    Update page layout and ordering of articles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,14 +21,19 @@ export default async function HomePage() {
         },
       },
     },
+    orderBy: {
+      created_at: 'desc',
+    },
+    take: -10,
   })
   return (
     <>
       <Inner>
         <header>
-          <h1 className="text-3xl mt-6 font-bold text-gray-800 dark:text-white">Articles</h1>
+          <h2 className="text-3xl font-bold text-gray-800 dark:text-gray-50">Articles</h2>
         </header>
-        <main className="my-6 grid grid-cols-1 gap-4 md:grid-cols-2">
+        <h3 className="text-xl mt-4 font-bold text-gray-800 dark:text-gray-50">最新</h3>
+        <main className="my-3 grid grid-cols-1 gap-4 md:grid-cols-2">
           {articles.map((article) => (
             <ArticleCard key={article.id} data={article} />
           ))}
