bd64b768c1bb228d8798845082917f134d554158
Riku-mono <riitkou@gmail.com>
2023-12-21 09:31:35 +0900

    Update Inner component's styling

--- src/components/Inner.tsx
+++ src/components/Inner.tsx
@@ -1,3 +1,3 @@
 export default function Inner({ children }: { children: React.ReactNode }) {
-  return <div className={`mx-auto max-w-7xl px-6 sm:px-8 lg:px-10`}>{children}</div>
+  return <div className={`mx-auto max-w-7xl px-6 my-10 sm:px-8 lg:px-10`}>{children}</div>
 }
