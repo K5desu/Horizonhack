c97c45e2c812c84f880335b6f5c6ff500593d97d
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2023-12-22 11:32:13 +0900

    add worksbackend

--- src/app/[username]/works/[id]/page.tsx
+++ src/app/[username]/works/[id]/page.tsx
@@ -1,14 +1,16 @@
 import Link from 'next/link'
-
+import Inner from '@/components/Inner'
 export default function UserWorkPage({ params }: { params: { username: string; id: string } }) {
   return (
     <>
-      <Link href={`../works`}>戻る</Link> <br />
-      <h1>User Work Detaile</h1>
-      <p>説明：成果物の詳細ページ</p>
-      <p>
-        {params.username} / works / {params.id}
-      </p>
+      <Inner>
+        <Link href={`../works`}>戻る</Link> <br />
+        <h1>User Work Detaile</h1>
+        <p>説明：成果物の詳細ページ</p>
+        <p>
+          {params.username} / works / {params.id}
+        </p>
+      </Inner>
     </>
   )
 }
