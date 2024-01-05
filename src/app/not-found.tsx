e9a64352e2a8487200ffa0961ddd3d1c3b773362
Riku-mono <riitkou@gmail.com>
2024-01-05 17:10:17 +0900

    Add NotFoundPage component

---
+++ src/app/not-found.tsx
@@ -0,0 +1,45 @@
+import Inner from '@/components/Inner'
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <Inner>
+        <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
+          <div className="wf-ull lg:w-1/2">
+            <p className="text-sm font-medium text-blue-500 dark:text-blue-400">404 error</p>
+            <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">
+              Page not found
+            </h1>
+            <p className="mt-4 text-gray-500 dark:text-gray-400">
+              お探しのページは見つかりませんでした。
+            </p>
+
+            <div className="flex items-center mt-6 gap-x-3">
+              <a
+                href="/"
+                className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 h-5 rtl:rotate-180"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                  />
+                </svg>
+
+                <span>Go back</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Inner>
+    </>
+  )
+}
