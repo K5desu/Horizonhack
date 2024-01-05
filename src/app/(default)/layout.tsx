94cef25c5231640a11fb6e3475e58e8f9a446229
Riku-mono <riitkou@gmail.com>
2024-01-05 17:28:14 +0900

    Add Toaster component and refactor RecentArticles rendering

--- src/app/(default)/layout.tsx
+++ src/app/(default)/layout.tsx
@@ -1,13 +1,17 @@
 import { getServerSession } from 'next-auth'
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
+import { Toaster } from '@/components/Toaster'
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession()
   return (
     <>
       <Header session={session} />
-      <div className="relative flex-1">{children}</div>
+      <div className="relative flex-1">
+        <Toaster position="bottom-center" reverseOrder={false} />
+        {children}
+      </div>
       <Footer />
     </>
   )
--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,34 +1,9 @@
-import prisma from '@/lib/prisma'
+import { Suspense } from 'react'
 import Inner from '@/components/Inner'
-import ArticleCard from '@/components/Article/ArticleCard'
+import RecentArticles from '@/components/Article/RecentArticles'
+import { ArticleCardSkeletons } from '@/components/Skeleton/skeletons'
 
 export default async function HomePage() {
-  const articles = await prisma.article.findMany({
-    where: {
-      visibility: true,
-    },
-    select: {
-      id: true,
-      title: true,
-      created_at: true,
-      author: {
-        select: {
-          name: true,
-          image: true,
-        },
-      },
-      tags: {
-        select: {
-          id: true,
-          name: true,
-        },
-      },
-    },
-    orderBy: {
-      created_at: 'desc',
-    },
-    take: -10,
-  })
   return (
     <>
       <Inner>
@@ -37,14 +12,9 @@ export default async function HomePage() {
         </header>
         <h3 className="text-xl mt-4 font-bold text-gray-800 dark:text-gray-50">最新</h3>
         <main className="my-3 grid grid-cols-1 gap-4 md:grid-cols-2">
-          {articles.map((article) => (
-            <ArticleCard key={article.id} data={article} />
-          ))}
-          {articles.length === 0 && (
-            <p className="text-gray-500 dark:text-gray-400">
-              まだ、記事がありません 最初の投稿者になりませんか？
-            </p>
-          )}
+          <Suspense fallback={<ArticleCardSkeletons />}>
+            <RecentArticles />
+          </Suspense>
         </main>
       </Inner>
     </>
