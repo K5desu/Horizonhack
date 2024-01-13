feb4fd8fcd8ef8c7cbeb04ac96508b44ac06aff6
Riku-mono <riitkou@gmail.com>
2024-01-13 16:30:25 +0900

    Fix imports and add Link component

--- src/app/(default)/[username]/articles/page.tsx
+++ src/app/(default)/[username]/articles/page.tsx
@@ -1,10 +1,10 @@
 import getUserArticlesAmount from '@/app/api/user/getUserArticlesAmount'
-import getUserProfile from '@/app/api/user/getUserProfile'
 import Inner from '@/components/Inner'
 import Pagination from '@/components/Pagination'
 import SortBtn from '@/components/SortBtn'
 import SearchUserArticles from '@/components/User/SearchArticles'
 import { getServerSession } from 'next-auth'
+import Link from 'next/link'
 
 export default async function UserArticlesPage({
   params,
@@ -34,11 +34,11 @@ export default async function UserArticlesPage({
       <>
         <Inner>
           <header>
-            <nav className="flex mb-2" aria-label="Breadcrumb">
+            <nav className="flex mb-3" aria-label="Breadcrumb">
               <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
                 <li className="inline-flex items-center">
-                  <a
-                    href="#"
+                  <Link
+                    href="/"
                     className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
                   >
                     <svg
@@ -51,7 +51,7 @@ export default async function UserArticlesPage({
                       <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
                     </svg>
                     Home
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <div className="flex items-center">
@@ -70,12 +70,12 @@ export default async function UserArticlesPage({
                         d="m1 9 4-4-4-4"
                       />
                     </svg>
-                    <a
+                    <Link
                       href={`/${params.username}`}
                       className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
                     >
                       {params.username}
-                    </a>
+                    </Link>
                   </div>
                 </li>
                 <li aria-current="page">
