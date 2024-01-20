6c1707cfecae6a224936de10f3139564985f62d5
Riku-mono <riitkou@gmail.com>
2024-01-21 06:30:41 +0900

    Update Tag component styling

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -5,13 +5,13 @@ export default function Tag({ name }: { name: string }) {
   return (
     <Link
       href={`/search?q=${encodedSearchWord}`}
-      className="group z-10 inline-flex cursor-pointer items-center gap-x-1 rounded-full border border-gray-300 bg-white px-2 py-1 text-xs font-medium text-gray-800 hover:bg-gray-50 dark:border-gray-700 dark:bg-slate-900 dark:text-white dark:hover:bg-slate-800"
+      className="group/tag z-10 inline-flex cursor-pointer items-center gap-x-1 rounded-full border border-gray-300 bg-white px-2 py-1 text-xs font-medium text-gray-800 hover:bg-gray-50 dark:border-gray-700 dark:bg-slate-900 dark:text-white dark:hover:bg-slate-800"
     >
       <p className=" text-slate-400" aria-hidden="true">
         #
       </p>
       <p
-        className="overflow-hidden overflow-ellipsis whitespace-nowrap group-hover:underline"
+        className="overflow-hidden overflow-ellipsis whitespace-nowrap group-hover/tag:underline"
         style={{ maxWidth: '8ch' }}
       >
         {name}
